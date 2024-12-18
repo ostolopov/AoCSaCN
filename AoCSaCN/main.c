99d309616bd874063a8b403d3fe9ac2ecbbbc4f3
ostolopov <egor21231bar@gmail.com>
2024-12-18 10:52:05 +0300

    Initial Commit

---
+++ AoCSaCN/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AoCSaCN
+//
+//  Created by Egor Baranov on 18.12.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
