d181ca89caab2aa8778d5829261826de23c49322
ostolopov <egor21231bar@gmail.com>
2024-12-18 11:11:34 +0300

    update

--- AoCSaCN/main.c
+++ AoCSaCN/main.c
@@ -1,14 +1,40 @@
-//
-//  main.c
-//  AoCSaCN
-//
-//  Created by Egor Baranov on 18.12.2024.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#define BUFFER 81
+
+typedef enum {
+    RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET, BLACK, WHITE
+} Color;
+
+typedef struct Object2D{
+    char type[BUFFER];
+    Color color;
+    union {
+        struct { int x, y, rad; } circle;      // круг
+        struct { int x1, y1, x2, y2; } rectangle; // прямоугольник
+        struct { int x1, y1, x2, y2, x3, y3; } triangle; // треугольник
+    } data;
+} Object2D;
+
+typedef struct Object3D{
+    char type[BUFFER];
+    double density;
+    union {
+        struct { int rad; } sphere;              // шар
+        struct { int length, width, height; } cuboid; // параллелепипед
+        struct { int length; } tetrahedron;            // правильный тетраэдр
+    } data;
+} Object3D;
+
+
+
+
+
+
+
+int main(void) {
+    
     return 0;
 }
